f32052211edc4211ef052b266f130f3858d32f93
Alexander Budanov <a@aspapro.com>
2021-11-29 20:10:34 +0200

    Update utils, routers

--- api/routers/assignments.py
+++ api/routers/assignments.py
@@ -26,3 +26,20 @@ async def assignments_questions(assignment_id: int, skip: int = 0, limit: int = 
                                 user: users.User = Depends(get_current_user)):
     records = await assignments_utils.get_assignments_questions(assignment_id=assignment_id)
     return {"total": len(records), "questions": records[skip:limit]}
+
+
+@router.get("/quizzes", response_model=List[assignments.QuizzesBase])
+async def get_assignments(user: users.User = Depends(get_current_user)):
+    return await assignments_utils.get_assignments_by_user(user=user)
+
+
+@router.get("/quizzes/{assignment_id}", response_model=assignments.QuizzesBase)
+async def assignments_details(assignment_id: int, user: users.User = Depends(get_current_user)):
+    return await assignments_utils.get_assignments_details(assignment_id=assignment_id)
+
+
+@router.get("/quizzes/{assignment_id}/questions")
+async def assignments_questions(assignment_id: int, skip: int = 0, limit: int = settings.QUESTION_PAGINATION_SIZE,
+                                user: users.User = Depends(get_current_user)):
+    records = await assignments_utils.get_assignments_questions(assignment_id=assignment_id)
+    return {"total": len(records), "questions": records[skip:limit]}
--- api/utils/assignments.py
+++ api/utils/assignments.py
@@ -1,4 +1,4 @@
-from api.models.assignments import assignments_table, questions_table
+from api.models.assignments import assignments_table, questions_table, quizzes_table
 from api.models.databases import database
 from api.models.students import students_table
 from api.schemas import users as user_schema
@@ -18,5 +18,23 @@ async def get_assignments_details(assignment_id: int):
 
 
 async def get_assignments_questions(assignment_id: int):
+    query = questions_table.select().where(questions_table.c.assignment_id == assignment_id).order_by('id')
+    return await database.fetch_all(query)
+
+
+async def get_quizzes_by_user(user: user_schema.User):
+    query = students_table.select().where(students_table.c.user_id == user.user_id)
+    students = await database.fetch_all(query)
+    students_ids = [student['id'] for student in students]
+    query = quizzes_table.select().where(assignments_table.c.student_id.in_(students_ids))
+    return await database.fetch_all(query)
+
+
+async def get_quizzes_details(assignment_id: int):
+    query = quizzes_table.select().where(assignments_table.c.ind_db == assignment_id)
+    return await database.fetch_one(query)
+
+
+async def get_quizzes_questions(assignment_id: int):
     query = questions_table.select().where(questions_table.c.assignment_id == assignment_id).order_by('id')
     return await database.fetch_all(query)
